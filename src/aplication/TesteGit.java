a5cba970400831aa81f47d27a20e1e7860dfd932
Lukebria <lucas.bria@gmail.com>
2019-03-06 11:38:29 -0300

    subindo essa budega de novo

--- src/aplication/TesteGit.java
+++ src/aplication/TesteGit.java
@@ -1,9 +1,14 @@
 package aplication;
 
+import java.util.Scanner;
+
 public class TesteGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("TESTE CARAIO");
+		scan.close();
 
 	}
 
